11fdc1c27c94850ba7d891af357e2fd15f34c041
sunyi-feng <9339996@qq.com>
2020-10-07 17:33:24 +0800

    增加第6章的注释

--- src/main/java/lambdasinaction/chap6/Dish.java
+++ src/main/java/lambdasinaction/chap6/Dish.java
@@ -6,10 +6,10 @@ import static java.util.Arrays.asList;
 
 public class Dish {
 
-    private final String name; //名称
-    private final boolean vegetarian; //是否素菜
-    private final int calories; //热量
-    private final Type type; //种类
+    private final String name; // 名称
+    private final boolean vegetarian; // 是否素菜
+    private final int calories; // 热量
+    private final Type type; // 种类
 
     public Dish(String name, boolean vegetarian, int calories, Type type) {
         this.name = name;
@@ -34,7 +34,9 @@ public class Dish {
         return type;
     }
 
-    // TODO 菜的种类枚举
+    /**
+     * 菜的种类枚举
+     */
     public enum Type { MEAT, FISH, OTHER }
 
     @Override
@@ -42,28 +44,34 @@ public class Dish {
         return name;
     }
 
+    /**
+     * 菜单列表
+     */
     public static final List<Dish> menu =
-            asList( new Dish("pork", false, 800, Dish.Type.MEAT),
+            Arrays.asList( new Dish("pork", false, 800, Dish.Type.MEAT),
                     new Dish("beef", false, 700, Dish.Type.MEAT),
                     new Dish("chicken", false, 400, Dish.Type.MEAT),
-                    new Dish("frenchFries", true, 530, Dish.Type.OTHER),//炸薯条
+                    new Dish("frenchFries", true, 530, Dish.Type.OTHER),// 炸薯条
                     new Dish("rice", true, 350, Dish.Type.OTHER),
-                    new Dish("seasonFruit", true, 120, Dish.Type.OTHER), //时令水果
+                    new Dish("seasonFruit", true, 120, Dish.Type.OTHER), // 时令水果
                     new Dish("pizza", true, 550, Dish.Type.OTHER),
-                    new Dish("prawns", false, 400, Dish.Type.FISH), //对虾
-                    new Dish("salmon", false, 450, Dish.Type.FISH)); //鲑鱼
+                    new Dish("prawns", false, 400, Dish.Type.FISH), // 对虾
+                    new Dish("salmon", false, 450, Dish.Type.FISH)); // 鲑鱼
 
+    /**
+     * 食物标签
+     */
     public static final Map<String, List<String>> dishTags = new HashMap<>();
 
     static {
-        dishTags.put("pork", asList("greasy", "salty"));
-        dishTags.put("beef", asList("salty", "roasted"));
-        dishTags.put("chicken", asList("fried", "crisp"));
-        dishTags.put("french fries", asList("greasy", "fried"));
-        dishTags.put("rice", asList("light", "natural"));
-        dishTags.put("season fruit", asList("fresh", "natural"));
-        dishTags.put("pizza", asList("tasty", "salty"));
-        dishTags.put("prawns", asList("tasty", "roasted"));
-        dishTags.put("salmon", asList("delicious", "fresh"));
+        dishTags.put("pork", asList("greasy", "salty")); // 油腻的, 咸咸的
+        dishTags.put("beef", asList("salty", "roasted")); // 咸咸的, 烧烤的
+        dishTags.put("chicken", asList("fried", "crisp")); // 油炸, 脆
+        dishTags.put("french fries", asList("greasy", "fried")); // 油腻的, 油炸
+        dishTags.put("rice", asList("light", "natural")); // 光亮，自然
+        dishTags.put("season fruit", asList("fresh", "natural")); // 新鲜，自然
+        dishTags.put("pizza", asList("tasty", "salty")); // 美味，咸味的
+        dishTags.put("prawns", asList("tasty", "roasted")); // 美味，烧烤
+        dishTags.put("salmon", asList("delicious", "fresh")); // 美味，新鲜
     }
 }
